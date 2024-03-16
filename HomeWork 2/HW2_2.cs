0110dc0603c88eb5d3c83492809e8e89cb9849c2
spart <spart@LAPTOP-EAHPQA0P>
2024-03-16 17:50:50 +0400

    Add project files.

---
+++ HomeWork 2/HW2_2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MainClass {
+    public static void Main (string[] args) {
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+        List<int> middleElements = FindMiddleElements(numbers);
+        Console.WriteLine("Middle element(s): ");
+        foreach (int num in middleElements) {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static List<int> FindMiddleElements(List<int> numbers) {
+        List<int> middleElements = new List<int>();
+        int length = numbers.Count;
+
+        if (length == 0) {
+            return middleElements;
+        }
+
+        if (length % 2 == 1) {
+            middleElements.Add(numbers[length / 2]);
+        } else {
+            middleElements.Add(numbers[length / 2 - 1]);
+            middleElements.Add(numbers[length / 2]);
+        }
+
+        return middleElements;
+    }
+}
---
+++ HomeWork 2/HW2_3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class MainClass
+{
+    public static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 1, 7, 3, 9, 5 };
+        int largestNumber = FindLargestNumber(numbers);
+        Console.WriteLine("The largest number is: " + largestNumber);
+    }
+
+    public static int FindLargestNumber(List<int> numbers)
+    {
+        if (numbers == null || numbers.Count == 0)
+        {
+            throw new ArgumentException("The list is null or empty.");
+        }
+        int largest = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        return largest;
+    }
+}
